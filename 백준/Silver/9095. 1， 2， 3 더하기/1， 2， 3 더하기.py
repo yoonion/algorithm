b58972a2c3cf966a3efb2daa57ec4742396b48b7
KIM SANGYOUN <yoonion97@gmail.com>
2024-02-27 17:32:36 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,31 +1,14 @@
 T = int(input())
 
-# 결과 담아서 마지막에 출력
-result = []
-
-def DFS(num, sum):
-    global count
-    # 더한 값이 구하는 값보다 크다면 return
-    if num < sum:
-        return
-
-    # 더한 값이 구한 값과 같다면 개수 추가
-    if num == sum:
-        count += 1
-        return
-
-    for i in range(1, 4):
-        sum += i
-
-        DFS(num, sum)
-        sum -= i
-
-# DFS 실행
 for _ in range(T):
-    num = int(input())
-    count = 0
-    DFS(num, 0)
-    result.append(count)
 
-# 결과 출력
-print('\n'.join(map(str, result)))
+    N = int(input())
+    dp = [0] * 12
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    print(dp[N])
