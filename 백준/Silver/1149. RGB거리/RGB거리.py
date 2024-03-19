6164b13204cfe94e21ef6d2864189b995978b1b9
KIM SANGYOUN <yoonion97@gmail.com>
2024-03-19 14:56:12 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -2,14 +2,14 @@ import sys
 
 N = int(sys.stdin.readline())
 
-rgb_list = []
+rgb = []
 for _ in range(N):
-    rgb_list.append(list(map(int, sys.stdin.readline().split())))
+    rgb.append(list(map(int, sys.stdin.readline().split())))
 
 # [[26, 40, 83], [49, 60, 57], [13, 89, 99]]
 for i in range(1, N):
-    rgb_list[i][0] = min(rgb_list[i - 1][1], rgb_list[i - 1][2]) + rgb_list[i][0]
-    rgb_list[i][1] = min(rgb_list[i - 1][0], rgb_list[i - 1][2]) + rgb_list[i][1]
-    rgb_list[i][2] = min(rgb_list[i - 1][0], rgb_list[i - 1][1]) + rgb_list[i][2]
+    rgb[i][0] = min(rgb[i - 1][1], rgb[i - 1][2]) + rgb[i][0]
+    rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][2]) + rgb[i][1]
+    rgb[i][2] = min(rgb[i - 1][0], rgb[i - 1][1]) + rgb[i][2]
 
-print(min(rgb_list[-1]))
+print(min(rgb[-1]))
