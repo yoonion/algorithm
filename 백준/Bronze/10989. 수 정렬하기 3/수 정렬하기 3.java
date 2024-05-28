9831cf62d18560b085cf7f1e7ca9e908a3fc5e58
KIM SANGYOUN <yoonion97@gmail.com>
2024-05-28 17:58:55 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2656 ms, Memory: 457248 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine()); //입력 숫자의 개수
+
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+        for (Integer num : nums) {
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
