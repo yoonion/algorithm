881624d9a10e6e68a0f341cf984d08b3977a9cb0
KIM SANGYOUN <yoonion97@gmail.com>
2024-05-29 18:15:24 +0900

    [Bronze I] Title: 단어 공부, Time: 476 ms, Memory: 29272 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -4,31 +4,28 @@ class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String inputStr = sc.next().toLowerCase();
 
-        String s = sc.nextLine();
-        String lowerCase = s.toLowerCase();
-
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < lowerCase.length(); i++) {
-            map.put(lowerCase.charAt(i), map.getOrDefault(lowerCase.charAt(i), 0) + 1);
+        int[] ints = new int[26];
+        for (int i = 0; i < inputStr.length(); i++) {
+            int alphaIndex = inputStr.charAt(i) - 97;
+            ints[alphaIndex] += 1;
         }
 
-        List<Character> keys = new ArrayList<>(map.keySet());
-        keys.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
-
-        Integer maxValue = Integer.MIN_VALUE;
-        Character maxResult = null;
-        for (Character key : keys) {
-            Integer value = map.get(key);
-            if (value.equals(maxValue)) {
-                maxResult = '?';
-                break;
-            } else if (value > maxValue) {
-                maxValue = value;
-                maxResult = key;
+        int maxIndex = 0;
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < ints.length; i++) {
+            if (ints[i] > maxValue) {
+                maxValue = ints[i];
+                maxIndex = i;
             }
         }
 
-        System.out.println(String.valueOf(maxResult).toUpperCase());
+        Arrays.sort(ints);
+        if (ints[ints.length - 1] == ints[ints.length - 2]) {
+            System.out.println("?");
+        } else {
+            System.out.println((char) (maxIndex + 65));
+        }
     }
 }
