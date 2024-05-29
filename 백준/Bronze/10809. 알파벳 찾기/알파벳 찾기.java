a66a664a720787a41b2ab9d2355aeced6b58dc3e
KIM SANGYOUN <yoonion97@gmail.com>
2024-05-29 16:47:24 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -4,10 +4,13 @@ class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
         String inputStr = sc.next();
         for (char c = 97; c <= 122; c++) {
-            System.out.print(inputStr.indexOf(c) + " ");
+            sb.append(inputStr.indexOf(c)).append(" ");
         }
+
+        System.out.println(sb);
     }
 }
