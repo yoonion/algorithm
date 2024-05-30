7ee82b24fad8a61b8c76375b5ab9fe77b15d981f
KIM SANGYOUN <yoonion97@gmail.com>
2024-05-30 15:51:10 +0900

    [Silver IV] Title: 카드2, Time: 264 ms, Memory: 48940 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            q.offer(q.poll());
+        }
+
+        System.out.println(q.poll());
+    }
+}
