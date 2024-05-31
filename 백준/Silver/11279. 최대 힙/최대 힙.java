d017acc367d69f12ad8b7e48c80e1a6937b7ca90
KIM SANGYOUN <yoonion97@gmail.com>
2024-05-31 15:16:04 +0900

    [Silver II] Title: 최대 힙, Time: 996 ms, Memory: 113992 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,27 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            int inputNum = sc.nextInt();
+            if (inputNum == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(inputNum);
+            }
+        }
+        System.out.println(sb);
+    }
+}
