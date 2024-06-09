fb94b39ba8e169a3b2eb73b0a3c6d8f76d75e57a
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-09 18:26:12 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2308 ms, Memory: 219596 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,33 +1,31 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
 
-        int N = sc.nextInt();
         int[][] nums = new int[N][2];
         for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            String[] s = sc.nextLine().split(" ");
+            int x = Integer.parseInt(s[0]);
+            int y = Integer.parseInt(s[1]);
             nums[i][0] = x;
             nums[i][1] = y;
         }
 
-        Arrays.sort(nums, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]) {
-                    return o1[1] - o2[1];
-                } else {
-                    return o1[0] - o2[0];
-                }
+        Arrays.sort(nums, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
             }
         });
 
         for (int[] num : nums) {
-            System.out.print(num[0] + " " + num[1] + "\n");
+            System.out.println(num[0] + " " + num[1]);
         }
-
     }
 }
