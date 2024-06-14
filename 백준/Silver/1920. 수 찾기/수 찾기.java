ce0b6a1f09af9e8304f1b1b963a8b6bff282bee7
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-14 17:17:22 +0900

    [Silver IV] Title: 수 찾기, Time: 1704 ms, Memory: 83084 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,15 +4,18 @@ class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int N = Integer.parseInt(sc.nextLine());
+
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < N; i++) {
-            set.add(sc.nextInt());
+        String[] nums = sc.nextLine().split(" ");
+        for (String num : nums) {
+            set.add(Integer.parseInt(num));
         }
 
-        int M = sc.nextInt();
-        for (int i = 0; i < M; i++) {
-            if (set.contains(sc.nextInt())) {
+        int M = Integer.parseInt(sc.nextLine());
+        String[] splits = sc.nextLine().split(" ");
+        for (String num : splits) {
+            if (set.contains(Integer.parseInt(num))) {
                 System.out.println(1);
             } else {
                 System.out.println(0);
