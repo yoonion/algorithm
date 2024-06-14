b88b12426b5129712ce871f2753a0bc99c76af46
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-14 17:29:01 +0900

    [Silver II] Title: 최소 힙, Time: 872 ms, Memory: 113044 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -6,21 +6,23 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-
         int N = sc.nextInt();
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
-            int inputNum = sc.nextInt();
-            if (inputNum == 0) {
+            int n = sc.nextInt();
+
+            if (n == 0) {
                 if (pq.isEmpty()) {
                     sb.append(0).append("\n");
                 } else {
                     sb.append(pq.poll()).append("\n");
                 }
             } else {
-                pq.offer(inputNum);
+                pq.offer(n);
             }
         }
+
         System.out.println(sb);
     }
 }
