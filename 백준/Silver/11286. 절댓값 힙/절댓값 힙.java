3ece5541479e0357ebb529c1ec36e3dadb1b3e8e
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-18 15:08:09 +0900

    [Silver I] Title: 절댓값 힙, Time: 324 ms, Memory: 25620 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                } else {
+                    return Math.abs(o1) - Math.abs(o2);
+                }
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(num);
+            }
+        }
+        br.close();
+        System.out.println(sb);
+    }
+}
