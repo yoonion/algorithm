e4a5829c68bae1731b6ab53158118d1f8f5525c7
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-18 15:31:19 +0900

    [Silver IV] Title: 문자열 집합, Time: 812 ms, Memory: 99004 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,21 +1,24 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String[] split = sc.nextLine().split(" ");
         int N = Integer.parseInt(split[0]);
         int M = Integer.parseInt(split[1]);
 
-        Set<String> S = new HashSet<>();
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < N; i++) {
-            S.add(sc.nextLine());
+            set.add(sc.nextLine());
         }
 
         int count = 0;
         for (int i = 0; i < M; i++) {
-            if (S.contains(sc.nextLine())) {
+            if (set.contains(sc.nextLine())) {
                 count++;
             }
         }
