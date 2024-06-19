0722997d9d8ed7065ebf546a79f4449d07981751
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-19 15:44:55 +0900

    [level 1] Title: 폰켓몬, Time: 3.90 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.58 ms
+메모리: 78.3 MB, 시간: 3.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 24일 22:17:43
+2024년 06월 19일 15:44:53
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        if (set.size() >= nums.length / 2) {
+            answer = nums.length / 2;
+        } else {
+            answer = set.size();
+        }
+        
+        return answer;
+    }
+}
