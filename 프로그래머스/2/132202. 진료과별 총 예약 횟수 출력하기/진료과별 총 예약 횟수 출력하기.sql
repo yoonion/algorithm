edabde10fd57fefcd1d94f0928ab8576172df757
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-24 17:41:35 +0900

    [level 2] Title: 진료과별 총 예약 횟수 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
+++ 프로그래머스/2/132202. 진료과별 총 예약 횟수 출력하기/진료과별 총 예약 횟수 출력하기.sql
@@ -1,5 +1,5 @@
-select mcdp_cd as 진료과코드, count(*) as 5월예약건수
-from APPOINTMENT
+select mcdp_cd as '진료과코드', count(*) as '5월예약건수'
+from appointment
 where apnt_ymd like '2022-05%'
 group by mcdp_cd
-order by COUNT(MCDP_CD), mcdp_cd
+order by count(mcdp_cd), mcdp_cd
