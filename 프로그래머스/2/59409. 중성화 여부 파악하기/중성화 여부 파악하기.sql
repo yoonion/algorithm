45a382ea61cf7b9a29d99e3f3832221e5a98d81f
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-24 17:33:30 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,3 +1,8 @@
-select animal_id, name, IF(SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%SPAYED%', 'O', 'X') AS '중성화'
+select animal_id, name, 
+    case 
+        when SEX_UPON_INTAKE like '%Neutered%' or SEX_UPON_INTAKE like '%Spayed%'
+        then 'O'
+        else 'X'
+    end as 중성화
 from animal_ins
 order by animal_id
