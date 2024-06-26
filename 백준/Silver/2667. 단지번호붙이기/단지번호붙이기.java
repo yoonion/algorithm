440ca0389b5674ebfe70dfa5bf26cd8601cd533e
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-26 18:12:39 +0900

    [Silver I] Title: 단지번호붙이기, Time: 232 ms, Memory: 17804 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,60 +1,59 @@
 import java.util.*;
 
 class Main {
-
+    
     static int[][] graph;
-    static int N;
-    static int[] dx = {0, 0, -1, 1};
+    static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
-    static int totalCount;
-    static List<Integer> eachCount = new ArrayList<>();
-
+    static int N;
+    static int totalCount = 0; // 단지(덩어리)의 개수
+    static List<Integer> list = new ArrayList<>(); // 각 단지의 집 개수 저장할 리스트
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = Integer.parseInt(sc.nextLine());
+
+        // 행렬 초기화
         graph = new int[N][N];
         for (int i = 0; i < N; i++) {
-            String[] s = sc.nextLine().split("");
-            for (int j = 0; j < s.length; j++) {
-                int n = Integer.parseInt(s[j]);
-                graph[i][j] = n;
+            String[] splits = sc.nextLine().split("");
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(splits[j]);
             }
         }
 
         BFS();
-        Collections.sort(eachCount);
+        list.sort(null);
 
         System.out.println(totalCount);
-        for (Integer n : eachCount) {
-            System.out.println(n);
+        for (Integer i : list) {
+            System.out.println(i);
         }
     }
 
     static void BFS() {
-        Queue<int[]> q = new LinkedList<>();
-        for (int x = 0; x < N; x++) {
-            for (int y = 0; y < N; y++) {
-                int count = 0;
-                if (graph[x][y] == 1) {
-                    graph[x][y] = 0;
-                    count++;
-                    totalCount++; // 총 단지 개수 증가
-                    q.offer(new int[]{x, y});
-                    while (!q.isEmpty()) {
-                        int[] poll = q.poll();
-                        int nowX = poll[0];
-                        int nowY = poll[1];
-                        for (int i = 0; i < 4; i++) {
-                            int nextX = nowX + dx[i];
-                            int nextY = nowY + dy[i];
-                            if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N && graph[nextX][nextY] == 1) {
+        Deque<int[]> dq = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (graph[i][j] == 1) {
+                    totalCount++;
+                    graph[i][j] = 0; // 방문처리
+                    dq.offer(new int[]{i, j});
+
+                    // 상, 하, 좌, 우 탐색하여 1인 집 탐색
+                    int count = 1;
+                    while (!dq.isEmpty()) {
+                        int[] poll = dq.poll();
+                        for (int k = 0; k < 4; k++) {
+                            int nx = poll[0] + dx[k];
+                            int ny = poll[1] + dy[k];
+                            if (nx >= 0 && nx < N && ny >= 0 && ny < N && graph[nx][ny] == 1) {
                                 count++;
-                                graph[nextX][nextY] = 0;
-                                q.offer(new int[]{nextX, nextY});
+                                graph[nx][ny] = 0; // 방문처리
+                                dq.offer(new int[]{nx, ny});
                             }
                         }
                     }
-                    eachCount.add(count);
+                    list.add(count);
                 }
             }
         }
