59ac37f97f54798a55feac5788de9e5e5b7d594b
KIM SANGYOUN <yoonion97@gmail.com>
2024-06-27 15:35:42 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1600 ms, Memory: 316596 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -21,27 +21,21 @@ class Main {
             graph[a][b] = graph[b][a] = 1;
         }
 
-        BFS();
+        for (int i = 1; i < N + 1; i++) {
+            if (visited[i] == 0) {
+                DFS(i);
+                answer++;
+            }
+        }
         System.out.println(answer);
     }
 
     // 그래프 BFS 탐색
-    static void BFS() {
-        Deque<Integer> dq = new ArrayDeque<>();
-        for (int i = 1; i < N + 1; i++) {
-            if (visited[i] == 0) {
-                visited[i] = 1;
-                dq.offer(i);
-                answer++;
-                while (!dq.isEmpty()) {
-                    Integer poll = dq.poll();
-                    for (int j = 1; j < N + 1; j++) {
-                        if (graph[poll][j] == 1 && visited[j] == 0) {
-                            dq.offer(j);
-                            visited[j] = 1;
-                        }
-                    }
-                }
+    static void DFS(int v) {
+        visited[v] = 1;
+        for (int i = 1; i < graph[v].length; i++) {
+            if (graph[v][i] == 1 && visited[i] == 0) {
+                DFS(i);
             }
         }
     }
