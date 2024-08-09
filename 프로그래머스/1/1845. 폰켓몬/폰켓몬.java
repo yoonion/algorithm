42f32c11f7d50bd2040f37c1295083b0acfd9e8a
KIM SANGYOUN <yoonion97@gmail.com>
2024-08-09 20:04:16 +0900

    [level 1] Title: 폰켓몬, Time: 5.23 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,19 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        
         Set<Integer> set = new HashSet<>();
+        int N = nums.length / 2;
+        
         for (int num : nums) {
             set.add(num);
         }
         
-        if (set.size() >= nums.length / 2) {
-            answer = nums.length / 2;
-        } else {
-            answer = set.size();
+        if (set.size() >= N) {
+            return N;
         }
         
-        return answer;
+        return set.size();
     }
 }
