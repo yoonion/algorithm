34e768401b3745cd241754615974a7513e04cc2f
KIM SANGYOUN <yoonion97@gmail.com>
2024-09-16 19:05:30 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,7 +1,7 @@
 select food_type, rest_id, rest_name, favorites
-from rest_info 
+from rest_info
 where (food_type, favorites) in (
-    select food_type, max(favorites)
+    select food_type, max(favorites) as favorites
     from rest_info
     group by food_type
 )
