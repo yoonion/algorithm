06ba686c0193bf976ae83a6c9f564fd1e34f6a63
KIM SANGYOUN <yoonion97@gmail.com>
2024-09-16 16:49:58 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,4 +1,4 @@
-select category, sum(bs.sales) as total_sales
+select category, sum(sales) as total_sales
 from book b join book_sales bs on b.book_id = bs.book_id
 where bs.sales_date like '2022-01%'
 group by category
