5f44533c3a2bd368089c25586e96b2a67d89c36e
KIM SANGYOUN <yoonion97@gmail.com>
2024-09-17 15:22:17 +0900

    [level 2] Title: 전화번호 목록, Time: 290.40 ms, Memory: 233 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,11 +2,17 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        Set<String> set = new HashSet<>();
         
-        for (int i=0; i<phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;    
+        for (String number : phone_book)  {
+            set.add(number);
+        }
+        
+        for (String number : phone_book)  {
+            for (int i=1; i<number.length(); i++) {
+                if (set.contains(number.substring(0, i))) {
+                    return false;
+                }
             }
         }
         
