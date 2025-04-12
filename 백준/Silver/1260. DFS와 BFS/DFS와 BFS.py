40a58352c8d8276551c24b7b772576e388659f1c
KIM SANGYOUN <yoonion97@gmail.com>
2025-04-12 20:56:37 +0900

    [Silver II] Title: DFS와 BFS, Time: 144 ms, Memory: 115348 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,47 +1,42 @@
 from collections import deque
 
-N, M, V = map(int, input().split())
+def dfs(n):
+    print(n, end=' ')
+    visited[n] = True
 
-graph = [[] for _ in range(N + 1)]
-for i in range(1, M + 1):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    for i in graph[n]:
+        if not visited[i]:
+            dfs(i)
 
-# 오름차순 출력을 위한 정렬
-for i in range(len(graph)):
+
+def bfs(n):
+    visited[n] = True
+    queue = deque([n])
+
+    while queue:
+        q = queue.popleft()
+        print(q, end=' ')
+
+        for i in graph[q]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+# 정점 번호 정렬
+for i in range(n + 1):
     graph[i].sort()
 
-dfs_visited = set()
-bfs_visited = set()
-
-dfs_answer = []
-bfs_answer = []
-def dfs(v):
-    if v in dfs_visited:
-        return
-
-    dfs_visited.add(v) # 방문 처리
-    dfs_answer.append(v)
-    for v in graph[v]:
-        dfs(v)
-dfs(V)
-
-def bfs(v):
-    q = deque()
-    q.append(v)
-
-    bfs_visited.add(v)
-    bfs_answer.append(v)
-
-    while q:
-        pop = q.popleft()
-        for v in graph[pop]:
-            if v not in bfs_visited:
-                bfs_answer.append(v)
-                bfs_visited.add(v)
-                q.append(v)
-bfs(V)
-
-print(" ".join(map(str, dfs_answer)))
-print(" ".join(map(str, bfs_answer)))
+visited = [False] * (n + 1)
+dfs(v)
+print()
+visited = [False] * (n + 1)
+bfs(v)
